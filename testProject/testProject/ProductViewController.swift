cb779362d80e650aeb0e768d6866c847a14e4be6
Frederik <frederikklovborg@Frederiks-MacBook-Pro.local>
2017-05-03 22:06:03 +0200

    Image
    Set image in code using imageLiteral

--- testProject/testProject/ProductViewController.swift
+++ testProject/testProject/ProductViewController.swift
@@ -11,11 +11,13 @@ import UIKit
 class ProductViewController: UIViewController {
 
     @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         productNameLabel.text = "2017 Volkswagen UP"
+        productImageView.image = #imageLiteral(resourceName: "up2017")
 
         // Do any additional setup after loading the view.
     }
